daa2e5037d36a7c6dee6638c5f9e394d40bd00bc
Azad Asanali <azadxperias2012@gmail.com>
2017-12-30 22:47:12 +0530

    object oriented principles - basic class design with access levels

---
+++ javaindepth/src/main/java/com/neotechlabs/objectoriented/Student.java
@@ -0,0 +1,37 @@
+package com.neotechlabs.objectoriented;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String gender = "male";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(int id, String name, String gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public static void main(String[] args) {
+        int[] studentIds = {1001, 1002, 1003};
+
+        Student student1 = new Student(studentIds[0], "joan", "male");
+        Student student2 = new Student(studentIds[1], "raj", "male");
+        Student student3 = new Student(studentIds[2], "anita", "female");
+
+        System.out.println("Name of student1: " + student1.getName());
+        System.out.println("Name of student2: " + student2.getName());
+        System.out.println("Name of student3: " + student3.getName());
+
+        student1.setName("john");
+        System.out.println("Updated name of student1: " + student1.getName());
+    }
+}
