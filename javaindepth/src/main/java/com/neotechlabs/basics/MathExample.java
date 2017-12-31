461b7a2125261e35d2898217a6acc64a7191810b
Azad Asanali <azadxperias2012@gmail.com>
2017-12-31 14:37:49 +0530

    math class demo

---
+++ javaindepth/src/main/java/com/neotechlabs/basics/MathExample.java
@@ -0,0 +1,19 @@
+package com.neotechlabs.basics;
+
+public class MathExample {
+    public static void main(String[] args) {
+
+        System.out.println(0.0/0.0);
+        System.out.println(Math.sqrt(-2.0));
+        System.out.println(Math.sqrt(2.0));
+        System.out.println((int)(Math.random() * 5));
+        System.out.println(Math.round(21.5));
+        System.out.println(Math.ceil(21.1));
+        System.out.println(Math.ceil(22));
+        System.out.println(Math.floor(21.1));
+        System.out.println(Math.ceil(23));
+        System.out.println(Math.max(23, 25));
+        System.out.println(Math.min(23, 25));
+
+    }
+}
