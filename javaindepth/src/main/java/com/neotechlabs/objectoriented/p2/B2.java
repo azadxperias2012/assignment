864dbc3bef1b82290b84c128e36a36b1b8c05f24
Azad Asanali <azadxperias2012@gmail.com>
2018-01-01 19:55:06 +0530

    inheritance and access modifiers example

---
+++ javaindepth/src/main/java/com/neotechlabs/objectoriented/p2/B2.java
@@ -0,0 +1,18 @@
+package com.neotechlabs.objectoriented.p2;
+
+import com.neotechlabs.objectoriented.p1.A1;
+
+public class B2 {
+    public static void main(String[] args) {
+//        System.out.println("A1.privateMember: " + A1.privateMember);
+//        System.out.println("A1.defaultMember: " + A1.defaultMember);
+//        System.out.println("C1.defaultMember: " + C1.defaultMember);
+
+//        System.out.println("A1.protectedMember: " + A1.protectedMember);
+//        System.out.println("C1.protectedMember: " + C1.protectedMember);
+//        System.out.println("C2.protectedMember: " + C2.protectedMember);
+
+        System.out.println("A1.publicMember: " + A1.publicMember);
+        System.out.println("C2.publicMember: " + C2.publicMember);
+    }
+}
