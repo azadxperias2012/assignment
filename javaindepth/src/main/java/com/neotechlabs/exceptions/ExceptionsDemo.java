28460305c59f5d807f2deeb442da8731205ca5ef
Azad Asanali <azadxperias2012@gmail.com>
2018-01-11 07:44:32 +0530

    Exception demo: finally block

--- javaindepth/src/main/java/com/neotechlabs/exceptions/ExceptionsDemo.java
+++ javaindepth/src/main/java/com/neotechlabs/exceptions/ExceptionsDemo.java
@@ -7,11 +7,14 @@ public class ExceptionsDemo {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("\nInside main ...");
-		//try {
+		try {
 			share();
-		//} catch (FileNotFoundException e) {
-			//e.printStackTrace();
-		//}
+			System.out.println("After invoking share ...");
+		} catch (FileNotFoundException e) {
+			System.out.println("main: FileNotFoundException ...");
+		} finally {
+			System.out.println("Inside main's finally ...");
+		}
 		System.out.println("\nEnd of main ...");
 	}
 
@@ -19,11 +22,15 @@ public class ExceptionsDemo {
 		System.out.println("\nInside share ...");
 
 		try {
-			HttpConnect.send(1, "hello", "http://www.goodsnips.com");
+			HttpConnect.send(0, "hello", "http://www.goodsnips.com");
+			System.out.println("After invoking send ...");
 		} catch (FileNotFoundException e) {
+			System.out.println("share: FileNotFoundException ...");
 			throw e;
 		} catch (IOException e) {
 			System.out.println("Connecting to a different server ...");
+		} finally {
+			System.out.println("Inside share's finally ...");
 		}
 
 		System.out.println("\nEnd of share ...");
