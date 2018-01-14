bedebad9336793d5367f408cec5d1d64c66f9acd
Azad Asanali <azadxperias2012@gmail.com>
2018-01-14 16:44:14 +0530

    IO Demo: Serializing Java Objects

--- javaindepth/src/main/java/com/neotechlabs/io/IODemo.java
+++ javaindepth/src/main/java/com/neotechlabs/io/IODemo.java
@@ -5,15 +5,35 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class IODemo {
+	
+	static String inFileStr = "walden.jpg";
+	static String outFileStr = "walden-out.jpg";
+	
+	// By convention, static nested classes should be placed before static methods
+	public static class SerializableDemo implements Serializable {
+		private String name;
+		public String getName() { return name; }
+		public void setName(String name) { this.name = name; }
+		
+		private transient int id = 4;
+		public int getId() { return id; }
+
+		private String gender;
+	}
+	
 	private static void applyEncoding() {
 		System.out.println("\nInside applyEncoding ...");
 
@@ -37,9 +57,6 @@ public class IODemo {
 			e.printStackTrace();
 		}
 	}
-
-	static String inFileStr = "walden.jpg";
-	static String outFileStr = "walden-out.jpg";
 
 	private static void fileCopyNoBuffer() {
 		System.out.println("\nInside fileCopyNoBuffer ...");
@@ -178,14 +195,53 @@ public class IODemo {
 		}
 	}
 
+	public void doSerialization() {
+		System.out.println("\nInside doSerialization ...");
+		
+		SerializableDemo serializableDemo = new SerializableDemo();
+		serializableDemo.setName("Java");
+		System.out.println("name (before serialization): " + serializableDemo.getName());
+		System.out.println("id (before serialization): " + serializableDemo.getId());
+		
+		try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("serial.ser")))) {
+			out.writeObject(serializableDemo);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doDeserialization() {
+		System.out.println("\nInside doDeserialization ...");
+		
+		try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("serial.ser")))) {
+			SerializableDemo serializedObj = (SerializableDemo) in.readObject();
+			System.out.println("name (after serialization): " + serializedObj.getName());
+			System.out.println("id (after serialization): " + serializedObj.getId());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		//applyEncoding();
 		//fileCopyNoBuffer();
 		//fileCopyWithBufferAndArray();
 		//readFromStandardInput();
 		//fileMethodsDemo();		
-		dirFilter(true);
-		dirFilter(false);
+		//dirFilter(true);
+		//dirFilter(false);
+		
+		// Serialization
+		if (args.length > 0 && args[0].equals("true")) {
+			new IODemo().doSerialization();
+		}
+		new IODemo().doDeserialization();
 	}
 }
 
