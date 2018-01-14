32af436a010e494c930bd65aab96ec8600ddca8e
Azad Asanali <azadxperias2012@gmail.com>
2018-01-14 11:44:45 +0530

    IO Demo: The file class

--- javaindepth/src/main/java/com/neotechlabs/io/IODemo.java
+++ javaindepth/src/main/java/com/neotechlabs/io/IODemo.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -122,11 +123,76 @@ public class IODemo {
 			System.out.println("Thanks. You entered code: " + code);
 		}
 	}
+	
+	private static void fileMethodsDemo() {
+		System.out.println("\nInside fileMethodsDemo ...");
+		
+		File f = new File("D:\\Project\\JavaPjt\\javaindepth\\src\\..\\walden.jpg"); // "movies\\movies.txt"
+		//File f = new File("walden.jpg");
+		
+		System.out.println("getAbsolutePath: " + f.getAbsolutePath());
+		try {
+			System.out.println("getCanonicalPath: " + f.getCanonicalPath());
+			System.out.println("createNewFile: " + f.createNewFile());
+		} catch (IOException e) {}
+		System.out.println("separator: " + f.separator);
+		System.out.println("separatorChar: " + f.separatorChar);
+		System.out.println("getParent: " + f.getParent());
+		System.out.println("lastModified: " + f.lastModified());
+		System.out.println("exists: " + f.exists());
+		System.out.println("isFile: " + f.isFile());
+		System.out.println("isDirectory: " + f.isDirectory());
+		System.out.println("length: " + f.length());
+		
+		System.out.println("My working or user directory: " + System.getProperty("user.dir"));
+		System.out.println("new File(\"testdir\").mkdir(): " + new File("testdir").mkdir());
+		System.out.println("new File(\"testdir\\test\").mkdir(): " + new File("testdir\\test").mkdir());
+		System.out.println("new File(\"testdir\").delete(): " + new File("testdir").delete());
+		System.out.println("new File(\"testdir\\test1\\test2\").mkdir(): " + new File("testdir\\test1\\test2").mkdir());
+		System.out.println("new File(\"testdir\\test1\\test2\").mkdirs(): " + new File("testdir\\test1\\test2").mkdirs());
+		
+		try {
+			File f2 = new File("temp.txt");
+			System.out.println("f2.createNewFile(): " + f2.createNewFile());
+			System.out.println("f2.renameTo(...): " + f2.renameTo(new File("testdir\\temp1.txt")));			
+		} catch (IOException e) {}
+	}
+
+	private static void dirFilter(boolean applyFilter) {
+		System.out.println("\nInside dirFilter ...");
+		
+		File path = new File(".");
+		String[] list;
+		
+		if (!applyFilter) {
+			System.out.println("Without filter ...");
+			list = path.list();
+		} else {
+			System.out.println("With filter ...");
+			list = path.list(new DirFilter());
+		}
+		
+		//Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+		for (String dirItem : list) {
+			System.out.println(dirItem);
+		}
+	}
 
 	public static void main(String[] args) {
 		//applyEncoding();
 		//fileCopyNoBuffer();
 		//fileCopyWithBufferAndArray();
-		readFromStandardInput();		
+		//readFromStandardInput();
+		//fileMethodsDemo();		
+		dirFilter(true);
+		dirFilter(false);
+	}
+}
+
+class DirFilter implements FilenameFilter {
+	// Holds filtering criteria
+	@Override
+	public boolean accept(File dir, String name) {
+		return name.toLowerCase().endsWith(".jpg");
 	}
 }
