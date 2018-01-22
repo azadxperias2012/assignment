5843d151f9d040c9fe753544c5ff367bed865c8e
Azad Asanali <azadxperias2012@gmail.com>
2018-01-22 21:08:31 +0530

    Generics demo: Item 23 - Dont use raw types in new code

--- javaindepth/src/main/java/com/neotechlabs/generics/GenericsDemo.java
+++ javaindepth/src/main/java/com/neotechlabs/generics/GenericsDemo.java
@@ -24,12 +24,46 @@ public class GenericsDemo<T extends ArrayList & Serializable> {
 		//GenericsDemo<List> list1 = new GenericsDemo<>();
 		GenericsDemo<ArrayList> list2 = new GenericsDemo<>();
 		//GenericsDemo<Collection> list3 = new GenericsDemo<>(); // Not possible to have class or subclass other than List
+		
+		// raw type demo
+		//rawTypeDemo();
+		
+		List<String> l1 = Arrays.asList("a", "b", "c");
+		List<String> l2 = Arrays.asList("b", "c", "d");
+		getCommonElemetsCount(l1, l2);
 	}
 	
+	private static int getCommonElemetsCount(List list1, List list2) {
+		int count = 0;
+		for (Object element : list1) {
+			if (list2.contains(element)) {
+				count++;
+			}
+		}
+		System.out.println("Common elements count: " + count);
+		return count;
+	}
+
+	private static void rawTypeDemo() {
+		System.out.println("\nInside rawTypeDemo ...");
+		int ISBN = 150529677;
+		List<Double> priceList = new ArrayList<>();
+		HalfIntegrator.getPrice(ISBN, priceList);
+		
+		Double price = priceList.get(0);
+		
+	}
+
 	public void go(T list) {
 		list.add(0, new Object());
 	}
 
+}
+
+class HalfIntegrator {
+	public static void getPrice(int ISBN, List priceList) {
+		priceList.add(45);
+	}
 }
 
 interface Container<T> {
