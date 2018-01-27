36c31f6e6c04a886cd76659a53b5eb65f1292969
Azad Asanali <azadxperias2012@gmail.com>
2018-01-27 18:44:21 +0530

    Enums Demo: enums with constant-specific behavior

--- javaindepth/src/main/java/com/neotechlabs/enums/Book.java
+++ javaindepth/src/main/java/com/neotechlabs/enums/Book.java
@@ -2,18 +2,42 @@ package com.neotechlabs.enums;
 
 public class Book {
 	public enum BookGenre {
-		BIOGRAPHY(12), HORROR(15);
+//		BIOGRAPHY(12),
+//		HORROR(15);
+
+		BIOGRAPHY(12) {
+			public boolean isKidFriendly(int age) {
+				return age >= minAgeToRead;
+			}
+		},
+		HORROR(15) {
+			public boolean isKidFriendly(int age) {
+				return false;
+			}
+		};
+		abstract boolean isKidFriendly(int age);
 
 		private BookGenre(int minAgeToRead) {
 			this.minAgeToRead = minAgeToRead;
 		}
 
-		private int minAgeToRead;
-
+		// access modifier should be changed for enum specific constant behavior
+		protected int minAgeToRead;
+		//private int minAgeToRead;
 		public int getMinAgeToRead() {
 			return minAgeToRead;
 		}
-
+		
+		/*
+		public boolean isKidFriendly(int age) {
+			switch (this) {
+				case BIOGRAPHY: return age >= minAgeToRead;
+				case HORROR: return false;
+			}
+			return false;
+			//throw new AssertionError("Unknown operation: " + this);
+		}
+		*/
 	}
 
 	public static void main(String[] args) {
@@ -25,6 +49,7 @@ public class Book {
 			System.out.print(", compareTo(HORROR): " + bookGenre.compareTo(BookGenre.HORROR));
 			System.out.print(", equals(HORROR): " + bookGenre.equals(BookGenre.HORROR));
 			System.out.print(", minAgeToRead: " + bookGenre.getMinAgeToRead());
+			System.out.print(", isKidFriendly: " + bookGenre.isKidFriendly(25));
 		}
 	}
 }
