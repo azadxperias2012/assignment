a5272c911dbcf76b8234d9a776246be96a50be17
Azad Asanali <azadxperias2012@gmail.com>
2018-01-30 00:48:02 +0530

    Concurrency Demo : Thread Coordination and Management

---
+++ javaindepth/src/main/java/com/neotechlabs/concurrency/indexer/IOUtil.java
@@ -0,0 +1,66 @@
+package com.neotechlabs.concurrency.indexer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class IOUtil {
+	public static void read(List<String> data, String filename) {
+		try (BufferedReader br = new BufferedReader(new
+				InputStreamReader(new FileInputStream(filename), "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				data.add(line);
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String read(InputStream in) {
+		StringBuilder text = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				text.append(line).append("\n");
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return text.toString();
+	}
+
+	public static void write(String webpage, long id) {
+		//D:\Project\JavaPjt\thrillio
+		String filename = "D:\\Project\\JavaPjt\\thrillio\\pages\\" + String.valueOf(id) + ".html";
+		try (BufferedWriter writer = new BufferedWriter(new
+				OutputStreamWriter(new FileOutputStream(filename), "UTF-8"))) {
+			writer.write(webpage);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
