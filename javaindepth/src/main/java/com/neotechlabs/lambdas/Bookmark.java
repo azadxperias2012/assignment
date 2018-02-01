a3e345f3497f942d2404df92cc1e5ecf2f4217f5
Azad Asanali <azadxperias2012@gmail.com>
2018-02-01 22:45:25 +0530

    Functional Pgm : Lamda

---
+++ javaindepth/src/main/java/com/neotechlabs/lambdas/Bookmark.java
@@ -0,0 +1,34 @@
+package com.neotechlabs.lambdas;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public class Bookmark {
+	private int id;
+	private String title;
+	private double rating;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+}
