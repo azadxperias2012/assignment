b88b5ef55c186ea15115e1d91b6c7537d9ad9253
Azad Asanali <azadxperias2012@gmail.com>
2018-02-07 08:34:12 +0530

    Functional Pgm: Slicing a stream

---
+++ javaindepth/src/main/java/com/neotechlabs/functional/streams/StreamOperations.java
@@ -0,0 +1,133 @@
+package com.neotechlabs.functional.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamOperations {
+	
+	static class Book {
+		private long isbn;
+		private String title;
+		private double rating;
+		private double price;
+		private String source;
+
+		public Book(long isbn, String title, double rating, double price, String source) {
+			this.isbn = isbn;
+			this.title = title;
+			this.rating = rating;
+			this.price = price;
+			this.source = source;
+		}
+
+		public long getIsbn() {
+			return isbn;
+		}
+
+		public void setIsbn(long isbn) {
+			this.isbn = isbn;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public double getRating() {
+			return rating;
+		}
+
+		public void setRating(double rating) {
+			this.rating = rating;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+		public String getSource() {
+			return source;
+		}
+
+		public void setSource(String source) {
+			this.source = source;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (int) (isbn ^ (isbn >>> 32));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Book other = (Book) obj;
+			if (isbn != other.isbn)
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Book [isbn=" + isbn + ", title=" + title + ", rating=" + rating + ", price=" + price + ", source="
+					+ source + "]";
+		}
+	}
+
+	public static void main(String[] args) {
+		List<Book> books = new ArrayList<>();
+
+		books.addAll(DataExtractor.getFromAmazon("java"));
+		books.addAll(DataExtractor.getFromBarnesAndNoble("java"));
+		
+		slice(books);
+	}
+
+	// Print at most 5 DISTINCT books with rating >= 4.5
+	// DB world: select distinct (ISBN) from book where rating >= 4.5 limit 0, 5;
+	private static void slice(List<Book> books) {
+		System.out.println("\nSlice ...\n");
+		
+		/*
+		List<String> result = books.stream()
+			.filter(d -> d.rating >= 4.5)
+			.distinct()
+			//.peek(d -> System.out.println("Peeking: " + d.getTitle() + ", Rating: " + d.getRating()))
+			.limit(5) // 5 top elements - short circuit operation
+			//.skip(5) // skips the first 5 elements
+			.map(d -> d.getTitle())
+			//.forEach(System.out::println);
+			.collect(Collectors.toList());
+		
+		for (String title : result) {
+			System.out.println("title: " + title);
+		}
+		*/
+		
+		Stream<Book> booksStream = books.stream()
+		.filter(d -> d.rating >= 4.5)
+		.distinct()
+		.limit(5);
+		
+		Stream<String> titleStream = booksStream.map(d -> d.getTitle());
+		titleStream.forEach(System.out::println);
+	}
+
+}
