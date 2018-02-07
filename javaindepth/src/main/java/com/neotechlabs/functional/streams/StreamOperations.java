24def883502c421a811414f966c5bff110738812
Azad Asanali <azadxperias2012@gmail.com>
2018-02-08 04:37:21 +0530

    Functional Pgm: Stream Matching Operations

--- javaindepth/src/main/java/com/neotechlabs/functional/streams/StreamOperations.java
+++ javaindepth/src/main/java/com/neotechlabs/functional/streams/StreamOperations.java
@@ -97,7 +97,35 @@ public class StreamOperations {
 		books.addAll(DataExtractor.getFromAmazon("java"));
 		books.addAll(DataExtractor.getFromBarnesAndNoble("java"));
 		
-		slice(books);
+		// intermediate operations (return stream objects)
+		//slice(books);
+		
+		// terminal operations (return non-stream objects)
+		match(books); // matching stream elements to some criteria
+		
+		// All matching and finding operations + limit
+		//		are short-circuit operations (recall &&, ||)
+	}
+
+	// Queries:
+	// 	(a) Is there at least one highly rated book (>= 4.8) that is inexpensive (<= 50.0)
+	// 	(b) Do all the books have a rating >= 4.8
+	// 	(c) Check if none of books have bad rating (2.0)?
+	private static void match(List<Book> books) {
+		System.out.println("\nMatching ...");
+		boolean anyMatch = books.stream()
+			.anyMatch(d -> d.getRating() >= 4.8 && d.getPrice() <= 50.0);
+		System.out.println("anyMatch? "+ anyMatch);
+		
+		boolean allMatch = books.stream()
+			.allMatch(d -> d.getRating() >= 4.8);
+			//.noneMatch(d -> d.getRating() < 4.8);
+		System.out.println("allMatch? "+ allMatch);
+		
+		boolean noneMatch = books.stream()
+			//.allMatch(d -> d.getRating() > 2.0);
+			.noneMatch(d -> d.getRating() <= 2.0);
+		System.out.println("noneMatch? "+ noneMatch);
 	}
 
 	// Print at most 5 DISTINCT books with rating >= 4.5
