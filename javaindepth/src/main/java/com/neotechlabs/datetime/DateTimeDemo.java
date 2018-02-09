f1fbf28e9796c683b43dbdc247d81a0d49a5c5d0
Azad Asanali <azadxperias2012@gmail.com>
2018-02-09 22:11:01 +0530

    Demo: New Date & Time API

--- javaindepth/src/main/java/com/neotechlabs/datetime/DateTimeDemo.java
+++ javaindepth/src/main/java/com/neotechlabs/datetime/DateTimeDemo.java
@@ -1,5 +1,17 @@
 package com.neotechlabs.datetime;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.Period;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -8,11 +20,75 @@ import java.util.TimeZone;
 public class DateTimeDemo {
 	
 	public static void main(String[] args) {
+		//testLegacyDateAPI();
+		testDateTimeAPI();
+	}
+
+	private static void testDateTimeAPI() {
+		// Use-case 1: Software renewal
+		LocalDate expiryDate = LocalDate.of(2017, Month.JUNE, 30);
+		System.out.println("expiryDate: " + expiryDate);
+		
+		// Other methods: plus & minus methods, isBefore, isAfter
+		LocalDate newExpiryDate = expiryDate.plusMonths(8);
+		System.out.println("newExpiryDate: " + newExpiryDate);
+		System.out.println("\nYear: " + newExpiryDate.getYear());
+		System.out.println("Month: " + newExpiryDate.getMonth());
+		System.out.println("day of month: " + newExpiryDate.getDayOfMonth());
+		System.out.println("day of week: " + newExpiryDate.getDayOfWeek());
+		System.out.println("Leap Year? " + newExpiryDate.isLeapYear());
+		System.out.println("length of month: " + newExpiryDate.lengthOfMonth());
+		
+		// get returns int
+		System.out.println("Year again: " + newExpiryDate.get(ChronoField.YEAR));
+		System.out.println("Month again: " + newExpiryDate.get(ChronoField.MONTH_OF_YEAR));
+		System.out.println("day of month again: " + newExpiryDate.get(ChronoField.DAY_OF_MONTH));
+		
+		// parse string
+		LocalDate epoch = LocalDate.parse("1970-01-01"); // yyyy-mm-dd
+		System.out.println("epoch year: " + epoch.getYear());
+		
+		// Use-case 2: Game start time localized to time zone
+		LocalTime time = LocalTime.of(9, 00, 00);
+		System.out.println("\ntime-hr: " + time.getHour());
+		
+		LocalDate gameStartDate = LocalDate.of(2017, Month.JULY, 03);
+		LocalDateTime gameStartTime = LocalDateTime.of(gameStartDate, time);
+		System.out.println("gameStartDateTime: " + gameStartTime);
+		
+		// TimeZone ==> ZoneId
+		ZonedDateTime zonedGameStartTime = ZonedDateTime.of(gameStartTime, ZoneId.of("Europe/London"));
+		System.out.println("zonedGameStartTime: " + zonedGameStartTime);
+		
+		ZonedDateTime gmt = zonedGameStartTime.withZoneSameInstant(ZoneId.of("GMT"));
+		System.out.println("gmt: " + gmt);
+		
+		ZonedDateTime indiaTime = zonedGameStartTime.withZoneSameInstant(ZoneId.of("Asia/Kolkata"));
+		System.out.println("indiaTime: " + indiaTime);
+		
+		ZonedDateTime pst = zonedGameStartTime.withZoneSameInstant(ZoneId.of("America/Los_Angeles"));
+		System.out.println("pst: " + pst);
+		
+		// Use-case 3: Age calculation (Period)
+		LocalDate birthDate = LocalDate.of(1989, Month.JULY, 21);
+		LocalDate today = LocalDate.now();
+		Period period = birthDate.until(today);
+		System.out.println("\nComplete Age: " + period.toString());
+		System.out.println("Age: " + period.getYears());
+		
+		// Use-case 4: Interval timing (Instant & Duration)
+		Instant startTime = Instant.now();
 		testLegacyDateAPI();
+		Instant endTime = Instant.now();
+		Duration timeElapsed = Duration.between(startTime, endTime);
+		System.out.println("timeElapsed in ms:" + timeElapsed.toMillis());
+		
+		// Partial Classes
+		System.out.println("Christmas: " + MonthDay.of(Month.DECEMBER, 25));
+		System.out.println("Credit card expiry date: " + YearMonth.of(2022, Month.FEBRUARY));
 	}
 
 	private static void testLegacyDateAPI() {
-
 		System.out.println("\nDate class ...");
 		Date currentDate = new Date();
 		System.out.println("currentDate: " + currentDate);
