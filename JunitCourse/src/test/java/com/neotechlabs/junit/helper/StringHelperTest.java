11c4806cf48b8446ce217b346d68e4c6b1c6d9e3
Azad Asanali <azadxperias2012@gmail.com>
2018-02-18 21:30:28 +0530

    Junit : Additional test cases and code refactored

--- JunitCourse/src/test/java/com/neotechlabs/junit/helper/StringHelperTest.java
+++ JunitCourse/src/test/java/com/neotechlabs/junit/helper/StringHelperTest.java
@@ -6,13 +6,27 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	StringHelper helper = new StringHelper();
+
+	// AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper();
-		// AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AinFirstPosition() {
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AnotInTheInput() {
 		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AinLast2Positions() {
 		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
 	}
 
