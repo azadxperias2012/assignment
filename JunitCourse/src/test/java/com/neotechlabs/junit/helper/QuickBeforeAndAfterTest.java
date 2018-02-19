8f16df23defe029daf899029928e6f9017bbf0d2
Azad Asanali <azadxperias2012@gmail.com>
2018-02-19 07:16:14 +0530

    Junit : Before and After Classes

--- JunitCourse/src/test/java/com/neotechlabs/junit/helper/QuickBeforeAndAfterTest.java
+++ JunitCourse/src/test/java/com/neotechlabs/junit/helper/QuickBeforeAndAfterTest.java
@@ -3,10 +3,18 @@ package com.neotechlabs.junit.helper;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class QuickBeforeAndAfterTest {
+	
+	@BeforeClass
+	public static void beforeClass() {
+		// Initialize static resources
+		System.out.println("Before Class");
+	}
 	
 	@Before
 	public void setup() {
@@ -26,6 +34,12 @@ public class QuickBeforeAndAfterTest {
 	@After
 	public void teardown() {
 		System.out.println("After test");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		// Clean up static resources
+		System.out.println("After Class");
 	}
 
 }
