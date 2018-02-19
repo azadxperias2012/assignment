15a19b8f4491c44a0a75d764330cdee6426498fe
Azad Asanali <azadxperias2012@gmail.com>
2018-02-19 07:24:21 +0530

    Junit : Unit test exceptions

--- JunitCourse/src/test/java/com/neotechlabs/junit/helper/ArraysCompareTest.java
+++ JunitCourse/src/test/java/com/neotechlabs/junit/helper/ArraysCompareTest.java
@@ -9,12 +9,18 @@ import org.junit.Test;
 public class ArraysCompareTest {
 
 	@Test
-	public void test() {
+	public void testArraySort_RandomArray() {
 		int[] numbers = {12, 3, 4, 1};
 		int[] expected = {1, 3, 4, 12};
 		
 		Arrays.sort(numbers);
 		assertArrayEquals(expected, numbers);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testArraySort_NullArray() {
+		int[] numbers = null;
+		Arrays.sort(numbers);
 	}
 
 }
