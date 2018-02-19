ac2ce9a425f72c5100372335a74f92431cd7b115
Azad Asanali <azadxperias2012@gmail.com>
2018-02-19 07:51:14 +0530

    Junit : Unit testing for performance

--- JunitCourse/src/test/java/com/neotechlabs/junit/helper/ArraysCompareTest.java
+++ JunitCourse/src/test/java/com/neotechlabs/junit/helper/ArraysCompareTest.java
@@ -22,5 +22,15 @@ public class ArraysCompareTest {
 		int[] numbers = null;
 		Arrays.sort(numbers);
 	}
+	
+	@Test(timeout = 20)
+	public void testArraySort_Performance() {
+		int[] array = {12, 23, 4};
+		
+		for (int i = 1; i <= 1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
 
 }
